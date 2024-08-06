6126571c3ee3b96140534b75a581d99f613d986d
Robert <robert@hokei.com>
2024-08-07 01:04:24 +0800

    Update and remove apollo

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,5 +1,3 @@
-import { ReadInventory } from '@/graphql/ClientQuery/ReadInventory';
-import { useQuery } from '@apollo/client';
 import React from 'react'
 import ImageGallery from "react-image-gallery";
 import 'react-image-gallery/styles/css/image-gallery.css';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { Icon } from '@iconify/react';
-import apolloClient from "../../lib/apollo";
 import Aside from "@/components/Partial/Aside";
 import Header from "@/components/Partial/Header";
 import { Head } from "next/document";
@@ -11,13 +10,11 @@ import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <ApolloProvider client={apolloClient}>
       <div className="flex flex-wrap h-[100vh]">
         <div className="flex flex-1 justify-center h-[8vh] align-center bg-gradient-to-t from-lime-500 via-lime-700 to-lime-800 z-50">
           <Header />
         </div>
         <Component {...pageProps} />;
         <Aside />
-      </div>
-    </ApolloProvider>)
+      </div>)
 }
