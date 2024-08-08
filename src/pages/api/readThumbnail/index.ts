b37807679b89fda1b4d743aeca9d76504af4d501
Robert <robert@hokei.com>
2024-08-08 11:35:54 +0800

    Update repository

--- src/pages/api/readThumbnail/index.ts
+++ src/pages/api/readThumbnail/index.ts
@@ -1,12 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../../lib/prisma";
-type Data = {
-  name: string;
-};
-
-export default async function handler(_req: NextApiRequest, res: NextApiResponse<Data>) {
-
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
   const result: any = await prisma.inventory.findMany({
     select: {
       id: true,
